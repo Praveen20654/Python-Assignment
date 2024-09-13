5ebeb495cd191a3276cea7881a1fc1e400956349
Praveen20654 <praveensriv95@gmail.com>
2024-09-13 20:53:34 +0530

    done

---
+++ variable.py
@@ -0,0 +1,11 @@
+# question 1
+
+
+print("-***---***-----***---***-----***---***-----***---***----")
+print("|                 Chitkara University                   |")
+print("|-------------------------------------------------------|")
+print("|              Name      :   Bob                        |")
+print("|              Degree    : Becholor of Engineering      |")
+print("-***---***-----***---***-----***---***-----***---***----")
+
+print("praveen")
